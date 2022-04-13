35cd6287baea38cdea07b1ad96643a74503e7b23
Phil Lewis <phillewis1985@gmail.com>
2022-04-13 13:02:28 -0600

    Add article details

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -2,27 +2,35 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Article = ({ news }) => {
-  const [ article, setArticle ] = useState()
+  const [ article, setArticle ] = useState({})
+  const [ loading, setLoading ] = useState(true)
   const params = useParams()
   
   useEffect(() => {
     const currentArticle = news.find(art => {
       return art.title = params.headline
     })
-    console.log(currentArticle)
     setArticle(currentArticle)
+    setLoading(false)
   }, [])
 
   const formatDate = (date) => {
-    
+    const published = date.split('T')[0]
+    const splitDate = published.split('-')
+    return `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
   }
 
   return (
-    <section>
-      <h2>{article.title}</h2>
-      {/* <p>{article.byline}</p> */}
-      <p></p>
-    </section>
+    <>
+      {!loading &&
+        <section>
+          <h2>{ article.title }</h2>
+          <p>{ article.byline }</p>
+          <p>{ formatDate(article.published_date) }</p>
+          <p>{ article.abstract }</p>
+        </section>
+      }
+    </>
   )
 }
 
