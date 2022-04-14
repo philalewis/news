c6bb0cc02afee9c1b527d2190aea29fb3c1fef01
Phil Lewis <phillewis1985@gmail.com>
2022-04-14 11:04:08 -0600

    Build Preview component for articles on Home page

---
+++ src/Components/Preview.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../Styles/Preview.css'
+
+const Preview = ({article}) => {
+  return (
+    <div className="story-container" key={article.url}>
+      <h3 className="headline" >{article.title}</h3>
+      <p className="abstract" >{article.abstract}</p>
+      <Link to={`/${article.title}`}>
+        <button className="details-btn">View Article</button>
+      </Link>
+    </div>
+  )
+}
+
+export default Preview
